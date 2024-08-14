e4dfb9c0ca5ddff6215cb48f1ca71d5940704743
Sproute <amaskeddev@gmail.com>
2024-08-14 08:44:00 +0300

    Added a while true main and a ctrl + c thing to exit

--- Source/client.py
+++ Source/client.py
@@ -1,15 +1,23 @@
 import socket
 
 IP = "127.0.0.1"
-PORT = 13063
+PORT = 13064
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((IP, PORT))
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((IP, PORT))
+    print(f"Connected to {IP}:{PORT}")
 
-print(f"Connected to {IP}:{PORT}")
-
-data = sock.recv(1024).decode()
-if data == "name":
-    sock.send(socket.gethostname().encode())
-
-sock.close()
+    while True:
+        data = sock.recv(1024).decode()
+        if data == "name":
+            sock.send(socket.gethostname().encode())
+        elif data == "exit":
+            sock.close()
+            exit(0)
+        else:
+            print(data)
+except KeyboardInterrupt:
+    print("\nClosing socket.")
+    sock.close()
+    exit(0)
--- Source/server.py
+++ Source/server.py
@@ -4,30 +4,58 @@ class Server:
     def __init__(self, ip, port):
         self.IP = ip
         self.PORT = port
-
+        self.controling = False
         self.sock = None
         self.bind()
     
     def bind(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind((self.IP, self.PORT))
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.bind((self.IP, self.PORT))
+            self.sock.listen(1)
+            print(f"Listening at {self.IP}:{self.PORT}.")
+            self.accept()
+        except KeyboardInterrupt:
+            self.cleanup()
 
-        self.sock.listen(1)
-        print(f"Listening at {IP}:{PORT}")
-
-        self.accept()
-    
     def accept(self):
-        conn, addr = self.sock.accept()
+        conn = None
+        try:
+            conn, addr = self.sock.accept()
+            conn.send(str("name").encode())
+            data = conn.recv(1024).decode()
+            print(f"Let's welcome '{data}'")
+            self.controling = True
+            self.accessing(conn, addr)
+        except KeyboardInterrupt:
+            self.cleanup()
 
-        conn.send(str("name").encode())
-        data = conn.recv(1024).decode()
+    def accessing(self, conn, addr):
+        try:
+            while self.controling:
+                userInput = input("Enter command: ").lower()
+                conn.send(userInput.encode())
+                if userInput == "exit":
+                    self.sock.close()
+                    exit(0)
 
-        print(f"welcome {data}")
-
-        self.sock.close()
+                data = conn.recv(1024).decode()
+                print(data)
+        except KeyboardInterrupt:
+            self.cleanup(conn)
     
+    def cleanup(self, conn=None):
+        self.controling = False
+
+        if conn:
+            conn.send("exit".encode())
+        if self.sock:
+            self.sock.close()
+
+        print("\nSocket closed.")
+        exit(0)
+
 IP = "127.0.0.1"
-PORT = 13063
+PORT = 13064
 
 Server(IP, PORT)
